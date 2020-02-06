b529ed9fd1cedec1cd0f3a93fbe1b3a12ee1e2ac
Amol Kumbhar <amolkum143@gmail.com>
2020-02-06 13:47:24 +0530

    flash screen code complted

--- app/src/main/java/com/ltrsoft/andromot/WelcomeActivity.java
+++ app/src/main/java/com/ltrsoft/andromot/WelcomeActivity.java
@@ -2,7 +2,10 @@ package com.ltrsoft.andromot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import static java.lang.Thread.sleep;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        Thread t=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    sleep(1000);
+
+                    Intent main_activity_intent=new Intent(WelcomeActivity.this,MainActivity.class);
+                    startActivity(main_activity_intent);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
     }
 }
